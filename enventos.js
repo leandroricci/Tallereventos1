5f2778aa57c66a612ae793786dbf3bce5d1903fe
leandroricci <leandroricci84@gmail.com>
2023-09-03 20:29:15 -0300

    Update enventos.js

--- enventos.js
+++ enventos.js
@@ -2,3 +2,11 @@
 function saludar() {
     alert("Hola!");
 }
+// eventos.js
+document.addEventListener("DOMContentLoaded", function () {
+    const divElement = document.querySelector("div");
+
+    divElement.addEventListener("click", function () {
+        alert("Hola! Soy el div");
+    });
+});
